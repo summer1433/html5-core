c447a9d8bbe028fa3053086781d309a2a49b6029
summer <summer.hot@gmail.com>
2011-08-19 15:44:10 +0800

    f

--- src/org.xiha.html5.core.scene.ext.js
+++ src/org.xiha.html5.core.scene.ext.js
@@ -4,14 +4,14 @@ org.xiha.html5.core.Scene.prototype.gravity = function(g) {
 	var self = this;
 	setInterval(function() {
 		for ( var i = 0; i < self.renderAble.length; i++) {
-			var o = self.renderAble[i];
-			var x = o.centerPosition.getX();
-			var y = o.centerPosition.getY();
-
-			if (y < self.getHeight() - o.getH() / 2) {
+			var cube = self.renderAble[i];
+			var x = cube.centerPosition.getX();
+			var y = cube.centerPosition.getY();
+			var finalY = self.getHeight() - (cube.getH() / 2);
+			if (y < finalY) {
 				y = y + 1;
-				o.addTrack(new org.xiha.html5.core.NormalPoint(x, y));
-				o.canRenderme();
+				cube.addTrack(new org.xiha.html5.core.NormalPoint(x, y));
+				cube.canRenderme();
 			}
 		}
 	}, 2);
